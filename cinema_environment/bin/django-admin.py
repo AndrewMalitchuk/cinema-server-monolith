40ea24c034ed1328dad83f244b3d26a6378417d3
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2020-01-03 22:20:38 +0200

    First initial commit. Setup an environment.

---
+++ cinema_environment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/adular/Projects/Python/Django/cinema-server/cinema_environment/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
